92aa75a771162d9f9f0321a238e808e626ad1541
Thais Martins <contato@thaismartins.co>
2018-08-24 17:37:56 -0300

    Adding custom styles

---
+++ src/theme/colors.js
@@ -0,0 +1,28 @@
+const white = '#FFFFFF';
+const black = '#0F131A';
+
+const neutrals = {
+  start: '#f5f7f9',
+  middle: '#dddddd',
+  end: '#f5f7f9'
+}
+
+const purples = {
+  start: '#582089',
+  middle: '#301167',
+  end: '#240c5d'
+}
+
+const oranges = {
+  start: '#dc5225',
+  middle: '#dc5225',
+  end: '#c11d2e'
+}
+
+export const colors = {
+  white,
+  black,
+  neutrals,
+  purples,
+  oranges
+};
---
+++ src/theme/fonts.js
@@ -0,0 +1,9 @@
+const weights = {
+  light: '300',
+  regular: '400',
+  bold: '700'
+}
+
+export const fonts = {
+  weights
+}
